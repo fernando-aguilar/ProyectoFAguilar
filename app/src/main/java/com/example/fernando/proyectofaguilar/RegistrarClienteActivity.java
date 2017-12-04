9b64f7836d54d6dbde90083f442318b09735b69f
Fernando <fernandexaguilar@gmail.com>
2017-12-04 00:49:33 -0400

    Firebase registro cliente

--- app/src/main/java/com/example/fernando/proyectofaguilar/RegistrarClienteActivity.java
+++ app/src/main/java/com/example/fernando/proyectofaguilar/RegistrarClienteActivity.java
@@ -1,5 +1,6 @@
 package com.example.fernando.proyectofaguilar;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,22 +8,32 @@ import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.fernando.proyectofaguilar.entidades.Cliente;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Calendar;
+
 public class RegistrarClienteActivity extends AppCompatActivity {
     DatabaseReference databaseCliente;
 
-    EditText txtNombreCliente, txtCorreoCliente;
+    EditText txtNombreCliente, txtCorreoCliente, txtFechaNacimiento;
+    Spinner ddlSexo;
     Button btnRegistrarCliente;
 
     private static final String LOG = RegistrarClienteActivity.class.getSimpleName();
     private Context context;
     private Toolbar toolbar;
+
+    static final int DATE_DIALOG_ID = 0;
+    private int año;
+    private int mes;
+    private int dia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +42,6 @@ public class RegistrarClienteActivity extends AppCompatActivity {
 
         //FireBase
         databaseCliente = FirebaseDatabase.getInstance().getReference("clientes");
-
 
         context = this;
         setTitle("Registrar Cliente");
@@ -43,6 +53,8 @@ public class RegistrarClienteActivity extends AppCompatActivity {
 
         txtNombreCliente = (EditText) findViewById(R.id.txtNombreCliente);
         txtCorreoCliente = (EditText) findViewById(R.id.txtCorreoCliente);
+        txtFechaNacimiento = (EditText) findViewById(R.id.txtFechaNacimiento);
+        ddlSexo = (Spinner) findViewById(R.id.ddlSexo);
         btnRegistrarCliente = (Button) findViewById(R.id.btnRegistrarCliente);
 
         btnRegistrarCliente.setOnClickListener(new View.OnClickListener() {
@@ -51,7 +63,28 @@ public class RegistrarClienteActivity extends AppCompatActivity {
                 adicionarCliente();
             }
         });
+
+//        txtFechaNacimiento.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                final Calendar c = Calendar.getInstance();
+//                año = c.get(Calendar.YEAR);
+//                mes = c.get(Calendar.MONTH);
+//                dia = c.get(Calendar.DAY_OF_MONTH);
+//
+//                showDialog(DATE_DIALOG_ID);
+//            }
+//        });
     }
+
+//    private DatePickerDialog.OnDateSetListener FechaListener =
+//            new DatePickerDialog.OnDateSetListener()
+//            {
+//                public void onDateSet(DatePicker view, int yearOf, int monthOfYear, int dayOfMonth)
+//                {
+//                    Toast.makeText(RegistrarClienteActivity.this, "Escogiste: " + dayOfMonth + "-" + monthOfYear + "-" + yearOf, Toast.LENGTH_SHORT).show();
+//                }
+//            };
 
     private void adicionarCliente() {
         String nombrecliente = txtNombreCliente.getText().toString().trim();
